b9191aab7d1506ad993943ee004823158f063743
Matias <tartaglia.matias3@gmail.com>
2023-04-13 12:12:52 -0300

    cola estatica testeada

--- TestingPila/src/lineales/estaticas/Cola.java
+++ TestingPila/src/lineales/estaticas/Cola.java
@@ -1,77 +1,74 @@
 package lineales.estaticas;
 
 public class Cola {
-    
-    private int tamanio=10;
+    private Object[]arreglo;
     private int frente;
     private int fin;
-    private Object[] array;
+    private static final int tamanio=10;
 
     public Cola(){
-        this.array = new Object[this.tamanio];
+        this.arreglo= new Object[this.tamanio];
         this.frente=0;
         this.fin=0;
     }
-
-    public boolean esVacia(){
-        return this.fin == this.frente;
-    }
-    public boolean poner(Object elemento){
-        boolean exit= false;
-        if(this.llena()){
-          exit=false;
-        }else{
-            array[fin]=elemento;
-            fin=(fin+1)%tamanio;
-            exit=true;
-        }
-        return exit;
-    }
-
-    private boolean llena(){
-        return (fin+1)%tamanio==frente;
-    }
-
-    public void vaciar() {
-        while (!esVacia()){
-            this.array[this.frente] = null;
-            this.frente = (this.frente+1)%tamanio;
-        }
-    }
     public boolean sacar(){
-        boolean exito=true;
-        if(this.esVacia()){
+        boolean exito=false;
+        if(!esVacia()){
+            this.arreglo[this.frente]=null;
+            this.frente=(this.frente+1)%tamanio;
             exito=true;
-        }else{
-            array[frente]=null;
-            fin= (fin+1)%tamanio;
         }
         return exito;
-    }
-    public String toString(){
-        String s="";
-        for(int i=0;i<tamanio;i++){
-            s=s+array[i];
-        }
-        return s;
     }
     public Object obtenerFrente(){
         Object obj = null;
         if (!esVacia()) {
-            obj = this.array[frente];
+            obj = this.arreglo[frente];
         }
         return obj;
     }
-    public Cola clone(){
-        Cola colaClon=new Cola();
+    public boolean poner(Object objIn) {
+        boolean exito = false;
+        if (!esllena()){
+            this.arreglo[this.fin] = objIn;
+            this.fin = (this.fin+1)%tamanio;
+            exito = true;
+        }
+        return exito;
+    }
+    public boolean esVacia() {
+        return this.fin == this.frente;
+    }
+    private boolean esllena(){
+        return (fin+1)%tamanio==frente;
+    }
+    public void vaciar(){
+        while(!esVacia()){
+            this.arreglo[this.frente]=null;
+            this.frente=(this.frente+1)%tamanio;
+        }
+    }
+    public Cola clone() {
+        Cola colaClon = new Cola();
         if(!esVacia()){
-            int i=this.frente;
-            while(i!=this.fin){
-                colaClon.array[i]=this.array[i];
-                i=(i+1)%tamanio;
+            int i = this.frente;
+            while (i != this.fin) {
+                colaClon.arreglo[i]=this.arreglo[i];
+                i=(i+1)%10;
             }
         }
+        colaClon.frente = this.frente;
+        colaClon.fin= this.fin;        
         return colaClon;
     }
-    
+    public String toString() {
+        int i = this.frente;
+        String cadena ="[|";
+        while (i != fin) {
+            cadena = cadena + this.arreglo[i] + "|";
+            i=(i+1)%10;
+        }
+        cadena = cadena + "]";
+        return cadena;
+    }
 }
