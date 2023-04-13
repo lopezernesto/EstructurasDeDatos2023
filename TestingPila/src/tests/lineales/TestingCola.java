90eb33db0fe8dce7a8067e76fd8a069cdb93e0ed
Matias <tartaglia.matias3@gmail.com>
2023-04-13 11:50:30 -0300

    arreglando cola estatica

--- TestingPila/src/tests/lineales/TestingCola.java
+++ TestingPila/src/tests/lineales/TestingCola.java
@@ -1,7 +1,7 @@
 
 package tests.lineales;
-//import lineales.estaticas.Cola;
- import lineales.dinamicas.Cola;
+import lineales.estaticas.Cola;
+// import lineales.dinamicas.Cola;
 
 public class TestingCola {
     
@@ -36,10 +36,16 @@ public class TestingCola {
 		}
 		System.out
 				.println("Recupera frente espera 1 recupera: \t\t\t" + (((int) q1.obtenerFrente() == 1) ? sOk : sErr));
-
+				
 		System.out.print("Saca espera true : \t\t\t\t\t" + ((q1.sacar() == true) ? sOk : sErr));
+
+
 		System.out.println("\t--> " + q1.toString());
-		System.out.println("Recupera frente espera 2 recupera \t\t\t" + (((int) q1.obtenerFrente() == 2) ? sOk : sErr));
+		
+		System.out.println("Recupera frente espera 2 recupera \t\t\t" + (((int) q1.obtenerFrente() == 2) ? sOk : sErr));//corta aca
+
+		
+
 		System.out.print("Saca espera true: \t\t\t\t\t" + ((q1.sacar() == true) ? sOk : sErr));
 		System.out.println("\t--> " + q1.toString());
 		System.out
