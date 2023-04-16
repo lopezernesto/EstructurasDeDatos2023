84df8032f4bf139ffbfb17193b593f3804ec4282
MatiasTarta <129552951+MatiasTarta@users.noreply.github.com>
2023-04-15 22:05:08 -0300

    Lista terminada

--- src/lineales/dinamicas/Lista.java
+++ src/lineales/dinamicas/Lista.java
@@ -77,13 +77,58 @@ public class Lista {
                 recuperado=aux.getElem();
         }
         return recuperado;
-    }
-    
+    } 
     public int localizar(Object buscado){
         int pos=-1;
-
-
-
+        if(!esVacia() || (pos>=1 && pos<=this.longitud)){
+            //Si mi lista no es vacia,  y pos es una posicion existente en la lista, entra al modulo
+                Nodo aux=this.cabecera;
+                int i=1;
+                while(aux.getElem()==buscado){
+                    //recorro la lista hasta llegar al elemento buscado
+                    aux=aux.getEnlace();
+                    i++;
+                }
+                //se corta al llegar al elemento buscado
+                pos=1;
+        }
         return pos;
+    }
+    public Lista clone(){
+        Lista listaClon= new Lista(this.longitud);
+        if(!esVacia()){
+            listaClon.cabecera = new Nodo(this.cabecera.getElem(), null);
+            //asigno a lista clon mi cabecera sin enlace
+            cloneAux(listaClon.cabecera, this.cabecera.getEnlace());
+            //clona los nodos recursivamente
+            listaClon.longitud = this.longitud;
+        }
+        return listaClon;
+    }
+    private void cloneAux(Nodo nodoTemp, Nodo nodoEnlace){
+        //modulo recursivo para clonar una estructuira dinamica de Nodos
+        //nodoTemp es usado para construir el clon, nodoEnlace es el nodo actual de la cola
+        if(nodoEnlace != null){
+            //condicion de salida que se llegue al final de la estructura
+            nodoTemp.setEnlace(new Nodo(nodoEnlace.getElem(),null));
+            //crea el siguiente nodo de nodoTemp con el valor del nodoEnlace.
+            cloneAux(nodoTemp.getEnlace(),nodoEnlace.getEnlace());
+            //llama al modulo con el nodo recien Creado y el siguiente nodo de la estructura
+        }
+    }
+    public String toString(){
+        String s="";
+        if(!esVacia()){
+            //Si mi lista no es vacia entra al modulo
+                Nodo aux=this.cabecera;
+                int i=0;
+                while(i<=longitud()){
+                    //recorro la lista hasta llegar a pos
+                    s=s+aux.getElem();
+                    aux=aux.getEnlace();
+                    i++;
+                } 
+        }
+        return s;
     }
 }
