3aa576af4f8ea0aa288194002d8dc97cfae20089
MatiasTarta <129552951+MatiasTarta@users.noreply.github.com>
2023-04-26 23:30:15 -0300

    creando arbol binario dinamioco

--- src/jerarquicas/Arbol.java
+++ src/jerarquicas/Arbol.java
@@ -1,5 +1,16 @@
 package jerarquicas;
 
 public class Arbol {
-    
+    private NodoArbol raiz;
+
+    public Arbol(){
+        this.raiz=null;
+    }
+    public boolean insertar(Object elemNuevi, Object elemPadre, char lugar){
+        boolean exito=false;
+
+        
+
+        return exito;
+    }
 }
