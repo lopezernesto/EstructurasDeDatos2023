ca13fb2a882421feb64dda10be4dce3040be415c
MatiasTarta <129552951+MatiasTarta@users.noreply.github.com>
2023-05-12 10:32:02 -0300

    nodoGen

--- src/jerarquicas/NodoGen.java
+++ src/jerarquicas/NodoGen.java
@@ -1,13 +1,31 @@
+package jerarquicas;
 
-class NodoGen{
+public class NodoGen {
     Object elem;
     NodoGen hijoIzquierdo;
     NodoGen hermanoDerecho;
 
-    NodoGen(Object elem, NodoGen hijoIzquierdo,NodoGen hermanoDerecho){
+    public  NodoGen(Object elem, NodoGen hijoIzquierdo,NodoGen hermanoDerecho){
         this.elem=elem;
         this.hijoIzquierdo=hijoIzquierdo;
         this.hermanoDerecho=hermanoDerecho;
     }
-    
+    public Object getElem(){
+        return this.elem;
+    }
+    public NodoGen getHijoIzquierdo(){
+        return this.hijoIzquierdo;
+    }
+    public NodoGen getHermanoDerecho(){
+        return this.hermanoDerecho;
+    }
+    public void setElem(Object elem){
+        this.elem=elem;
+    }
+    public void setHijoIzquierdo(NodoGen hijoIzquierdo){
+        this.hijoIzquierdo=hijoIzquierdo;
+    }
+    public void setHermanoDerecho(NodoGen hermanoDerecho){
+        this.hermanoDerecho=hermanoDerecho;
+    }
 }
